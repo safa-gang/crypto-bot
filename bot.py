6a8418fe3ac3172c609c693419260ded325826cd
safa-gang <safaeimahdi200@gmail.com>
2025-05-02 00:33:51 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 import telebot
 import requests
 
-TOKEN = "7505752036:AAGYB1DUL7ZP5wO7RlR5YYbArf-yl_5ur3s"
-
+TOKEN = '7505752036:AAGYB1DUL7ZP5wO7RlR5YYbArf-yl_5ur3s'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام داداش صفا! خوش اومدی به ربات 🫡")
+    bot.reply_to(message, "سلام! برای دیدن قیمت بیت‌کوین دستور /price رو بزن 😊")
 
 @bot.message_handler(commands=['price'])
 def send_price(message):
     try:
         response = requests.get("https://api.coincap.io/v2/assets/bitcoin")
+        response.raise_for_status()  # بررسی وضعیت HTTP
         data = response.json()
         price = data['data']['priceUsd']
         bot.reply_to(message, f"Bitcoin Price (Coincap): ${float(price):,.2f}")
     except Exception as e:
-        bot.reply_to(message, "Error fetching price. Try again later.")
+        bot.reply_to(message, f"Error fetching price. Try again later.\n{e}")
 
-bot.infinity_polling()
+print("ربات روشنه")
+bot.polling()
